e8465d6a932ed819d99845e5a2b3af0f8a7f9c01
Abel Akmal <abelakmal06@gmail.com>
2023-10-24 10:36:07 +0700

    membuat object dan array

--- day1.js
+++ day1.js
@@ -1,31 +1,64 @@
-//frist time belajar 
-console.info("hello world")
+// //frist time belajar
+// console.info('hello world');
+
+// // membuat variabel
+// let message;
+// message = 'hello word';
+
+// let error = 'error message';
+
+// if (true) {
+//   console.log(message);
+// } else {
+//   console.log(error);
+// }
+
+// //jenis-jenis varriabel
+// var globalVariabel = 'digunakan untuk global';
+// // var globalVariabel = "oke" jika namanya sama maka variabel awal akan ditimpa variabel baru
+// let blockVariabel = 'digunakan untuk variabel block';
+// const constanVariabel = 'digunakan untuk variabel yang tidak berubah rubah';
+
+// console.info(`
+//  var = ${globalVariabel}
+//  let = ${blockVariabel} 
+//  const = ${constanVariabel}`);
+
+// //Type Data
+
+// //String
+// let string = 'didalam ini berisi string';
+
+// //number
+// let angka = 2883;
+
+// //boolean => true/false
+// let boolean = true;
+
+// //null
+
+// let empty = null;
+
+// console.log(typeof null);
+// console.log(typeof Object);
+// console.log(typeof Array);
+// console.log(empty === Object);
+
+// //data non-primitive object array
+
+// let person = {
+//   nama: '',
+//   alamat: 'indonesia',
+//   umur:0
+// };
+
+// person.nama = "abel"
+// person.umur = 18
 
 
-// membuat variabel
-let message;
-message ="hello word";
 
-let error = "error message";
+// console.log(person);
 
-if(true){
-    console.log(message)
-}else{
-    console.log(error);
-}
-
-//jenis-jenis varriabel
-var globalVariabel = "digunakan untuk global"
-// var globalVariabel = "oke" jika namanya sama maka variabel awal akan ditimpa variabel baru
-let blockVariabel ="digunakan untuk variabel block"
-const constanVariabel = "digunakan untuk variabel yang tidak berubah rubah";
-
-console.info(`
- var = ${globalVariabel}
- let = ${blockVariabel} 
- const = ${constanVariabel}`)
+// let data = ["abel","akmal"];
 
 
-//STRING
-
-let stringMessage = ""
