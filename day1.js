ff3847c665768bdc6e9e6520aa94c7bfa88f3450
Abel Akmal <abelakmal06@gmail.com>
2023-10-24 10:16:14 +0700

    tentang string

--- day1.js
+++ day1.js
@@ -1,1 +1,31 @@
-console.log("hello world")
+//frist time belajar 
+console.info("hello world")
+
+
+// membuat variabel
+let message;
+message ="hello word";
+
+let error = "error message";
+
+if(true){
+    console.log(message)
+}else{
+    console.log(error);
+}
+
+//jenis-jenis varriabel
+var globalVariabel = "digunakan untuk global"
+// var globalVariabel = "oke" jika namanya sama maka variabel awal akan ditimpa variabel baru
+let blockVariabel ="digunakan untuk variabel block"
+const constanVariabel = "digunakan untuk variabel yang tidak berubah rubah";
+
+console.info(`
+ var = ${globalVariabel}
+ let = ${blockVariabel} 
+ const = ${constanVariabel}`)
+
+
+//STRING
+
+let stringMessage = ""
