fc15432ddcee94c940da59ff918e3758e74e786d
Abel Akmal <abelakmal06@gmail.com>
2023-10-25 15:13:36 +0700

    day2 belajar

--- ecercies/exercie-day2.js
+++ ecercies/exercie-day2.js
@@ -7,32 +7,34 @@ console.log(fahrenhit);
 
 //nomor 2
 
-let number = 24;
-
-let genap;
-let ganjil;
+let number = 20;
+let message1;
 
 if (number % 2 === 0) {
-  console.log('odd number');
+  message1 = 'angka genap';
 } else {
-  console.log('even number');
+  message1 = 'angka ganjil';
 }
 
+console.log(message1)
+
 //nomor 3
-let angka = 5;
+let angka = 4;
 let a = 0;
-let result = '';
+let message2 = '';
 for (let x = 0; x <= angka; x++) {
-  if (angka % x === 0 && angka > 1) {
-    a++;
-  }
-  if (a === 2) {
-    result = ' sebuah angka prime';
-  } else {
-    result = 'bukan sebuah angka prime';
+  if (angka > 0 && angka > 1) {
+    if (angka % x === 0) {
+      a++;
+    }
+    if (a === 2) {
+      message2 = ' sebuah angka prime';
+    } else {
+      message2 = 'bukan sebuah angka prime';
+    }
   }
 }
-console.log(result);
+console.log('\n ' + message2);
 
 //nomor 4
 
@@ -43,7 +45,7 @@ for (let y = 0; y <= angkaSum; y++) {
   resultSum += y;
 }
 
-console.log(resultSum);
+console.log('result = ' + resultSum);
 
 //nomor 5
 let angkaFaktorial = 3;
@@ -52,7 +54,7 @@ for (let z = 1; z <= angkaFaktorial; z++) {
   resultFaktorial *= z;
 }
 
-console.log(resultFaktorial);
+console.log('\n' + resultFaktorial);
 
 //nomor 6
 
@@ -61,12 +63,14 @@ let N = 15;
 let fbnc1 = 0;
 let fbnc2 = 1;
 let fbnc3;
+let resultFbnc;
 
-for (let i = 1; i < N ; i++) {
-    fbnc3 = fbnc1 + fbnc2;
-    fbnc1 =  fbnc2;
-    fbnc2 = fbnc3;
-    console.log(fbnc1 + " + " + fbnc2 + " = " + fbnc3)
+for (let i = 1; i < N; i++) {
+  fbnc3 = fbnc1 + fbnc2;
+  fbnc1 = fbnc2;
+  fbnc2 = fbnc3;
+  resultFbnc = fbnc3;
+  console.log(`${fbnc1} + ${fbnc2} = ${fbnc3} \n`);
 }
 
-
+console.log(`result = ${resultFbnc}`);
---
+++ materi/day2.js
@@ -0,0 +1,23 @@
+let umur = 17;
+let punyaSim = true;
+
+if(umur >= 17 && punyaSim){
+    console.log("bisa bawah kendaraan")
+}else{
+    console.log("tidak bisa bawah kendaraan")
+}
+
+for(let i = 0; i < 10; i++){
+    console.log(i*2);
+}
+for(let i = 10; i >= 0; i--){
+    if(i% 2 === 0){
+        console.log(i);
+    }
+}
+
+while(true){
+}
+do{
+    
+}while(true)
