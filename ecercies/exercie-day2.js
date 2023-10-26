426cfd313f49dff3c31ced168580cf0137d2d72b
Abel Akmal <abelakmal06@gmail.com>
2023-10-26 13:44:17 +0700

    exercise day2 update

--- ecercies/exercie-day2.js
+++ ecercies/exercie-day2.js
@@ -1,13 +1,16 @@
 //nomor 1
 
-const celsiust = 90;
-const fahrenhit = (celsiust * 9) / 5 + 32;
+const celsiust = 60;
+const celsiustToFahrenhit = (celsiust * 9) / 5 + 32;
 
-console.log(fahrenhit);
+console.log(`fahrenhit = ${celsiustToFahrenhit}`);
+
+const fahrenhitTocelciust = ((celsiustToFahrenhit - 32) * 5) / 9;
+console.log(`celciust = ${celsiust}`);
 
 //nomor 2
 
-let number = 20;
+let number = 25;
 let message1;
 
 if (number % 2 === 0) {
@@ -16,23 +19,23 @@ if (number % 2 === 0) {
   message1 = 'angka ganjil';
 }
 
-console.log(message1)
+console.log(message1);
 
 //nomor 3
-let angka = 4;
+let angka = 2;
 let a = 0;
 let message2 = '';
 for (let x = 0; x <= angka; x++) {
-  if (angka > 0 && angka > 1) {
-    if (angka % x === 0) {
-      a++;
-    }
-    if (a === 2) {
-      message2 = ' sebuah angka prime';
-    } else {
-      message2 = 'bukan sebuah angka prime';
-    }
+  // if (angka > 0 && angka > 1) {
+  if (angka % x === 0) {
+    a++;
   }
+  if (a === 2) {
+    message2 = ' sebuah angka prime';
+  } else {
+    message2 = 'bukan sebuah angka prime';
+  }
+  // }
 }
 console.log('\n ' + message2);
 
@@ -40,21 +43,27 @@ console.log('\n ' + message2);
 
 let angkaSum = 5;
 let resultSum = 0;
+let message = '';
 
-for (let y = 0; y <= angkaSum; y++) {
+for (let y = 1; y <= angkaSum; y++) {
   resultSum += y;
+  message += y + (y !== angkaSum ? ' + ' : '');
 }
 
-console.log('result = ' + resultSum);
+console.log(`${message} = ${resultSum}`);
 
 //nomor 5
-let angkaFaktorial = 3;
+let angkaFaktorial = 6;
 let resultFaktorial = 1;
-for (let z = 1; z <= angkaFaktorial; z++) {
+let messageFaktorial = '';
+
+for (let z = angkaFaktorial; z >= 1; z--) {
   resultFaktorial *= z;
+  console.log(z)
+  messageFaktorial += z + (z !== 1 ? ' x ' : ' = ');
 }
 
-console.log('\n' + resultFaktorial);
+console.log(`${angkaFaktorial}! -> ${messageFaktorial}${resultFaktorial}`);
 
 //nomor 6
 
@@ -63,14 +72,14 @@ let N = 15;
 let fbnc1 = 0;
 let fbnc2 = 1;
 let fbnc3;
-let resultFbnc;
+let messageFbnc = "";
 
 for (let i = 1; i < N; i++) {
-  fbnc3 = fbnc1 + fbnc2;
-  fbnc1 = fbnc2;
-  fbnc2 = fbnc3;
-  resultFbnc = fbnc3;
-  console.log(`${fbnc1} + ${fbnc2} = ${fbnc3} \n`);
+  // fbnc3 = fbnc1 + fbnc2;
+  // messageFbnc += fbnc1 + " ";
+  // fbnc1 = fbnc2;
+  // fbnc2 = fbnc3;
+  [fbnc1 , fbnc2] = [fbnc2 , fbnc1 + fbnc2]
 }
 
-console.log(`result = ${resultFbnc}`);
+console.log(`${fbnc1}`)
